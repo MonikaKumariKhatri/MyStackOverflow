dd0411c7a3ffe3961ab470d5dcea77cfb3a8b22d
MonikaKumariKhatri <43932176+MonikaKumariKhatri@users.noreply.github.com>
2019-09-01 22:42:29 +0530

    Add files via upload
    added caching

--- src/containers/logic.js
+++ src/containers/logic.js
@@ -1,15 +1,22 @@
 import axios from "axios";
 import { store } from "./../store";
 import { actions as searchResultsAction } from "../ducks/searchResults";
+import { defaultMemoize } from "reselect";
+import cache from "./cacheLRU";
 
-export const getResponseLogic = async query => {
-  const response = await axios.get(
-    `https://api.stackexchange.com/2.2/search/excerpts?order=desc&sort=activity&q=${query}&site=stackoverflow`
-  );
+export const getResponseLogic = defaultMemoize(async query => {
+  let response = cache.get(query);
+  if (!response) {
+    response = await axios.get(
+      `https://api.stackexchange.com/2.2/search/excerpts?order=desc&sort=activity&q=${query}&site=stackoverflow`
+    );
+    cache.set(query, response);
+  }
+
   await store.dispatch(
     searchResultsAction.replaceSearchResults(response.data.items)
   );
-};
+});
 
 export const decodeHtml = html => {
   var txt = document.createElement("textarea");
